90b3b0405d65efbf58bb20aa9849005157db068c
Lorena2387 <lorenaramirez23@hotmail.com>
2024-09-01 12:35:24 -0500

    implement tabBar in the appBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,18 +7,52 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Whatsapp clone'),
+        backgroundColor: Color(0xFF075E54),
+        title: Text('Whatsapp'),
         actions: [
           IconButton(
             onPressed: () {},
             icon: Icon(Icons.search),
           ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.more_vert,
+            ),
+          ),
         ],
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: "CHATS",
+            ),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS",
+            ),
+          ],
+        ),
       ),
     );
   }

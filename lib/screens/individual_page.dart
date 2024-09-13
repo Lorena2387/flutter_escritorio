17bc8fc17594f87c94a8169c9c4223d18f35d5e1
Lorena2387 <lorenaramirez23@hotmail.com>
2024-09-12 19:43:57 -0500

    implement camera and call icons and a menu in the appBar of the individual_page.dart screen

--- lib/screens/individual_page.dart
+++ lib/screens/individual_page.dart
@@ -17,6 +17,7 @@ class _IndividualPageState extends State<IndividualPage> {
       appBar: AppBar(
         backgroundColor: Color(0xFF075E54),
         leadingWidth: 70,
+        titleSpacing: 0,
         leading: InkWell(
           onTap: () {
             Navigator.pop(context);
@@ -29,12 +30,70 @@ class _IndividualPageState extends State<IndividualPage> {
                 size: 24,
               ),
               CircleAvatar(
+                child: Image.asset(
+                  widget.chatModel.isGropup
+                      ? "assets/images/groups.png"
+                      : "assets/images/person.png",
+                ),
                 radius: 20,
                 backgroundColor: Colors.blueGrey,
               )
             ],
           ),
         ),
+        title: InkWell(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(widget.chatModel.name),
+                Text(
+                  "Last seen today at 12:05",
+                  style: TextStyle(fontSize: 15.5),
+                )
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.videocam),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.call),
+          ),
+          PopupMenuButton<String>(onSelected: (value) {
+            print(value);
+          }, itemBuilder: (BuildContext context) {
+            return [
+              PopupMenuItem(
+                child: Text("View contact"),
+                value: "View contact",
+              ),
+              PopupMenuItem(
+                child: Text("Media, links, and docs"),
+                value: "Media, links, and docs",
+              ),
+              PopupMenuItem(
+                child: Text("Watsapp web"),
+                value: "Whatsapp web",
+              ),
+              PopupMenuItem(
+                child: Text("Search"),
+                value: "Search",
+              ),
+              PopupMenuItem(
+                child: Text("Mute notification"),
+                value: "Mute notification",
+              ),
+            ];
+          })
+        ],
       ),
     );
   }

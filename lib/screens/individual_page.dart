632ef8235d1a7567657a2f002070d8cdcc794ac0
Lorena2387 <lorenaramirez23@hotmail.com>
2024-09-15 19:02:08 -0500

    implement a container inside the container a card and inside the card a textFormField

--- lib/screens/individual_page.dart
+++ lib/screens/individual_page.dart
@@ -14,86 +14,114 @@ class _IndividualPageState extends State<IndividualPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF075E54),
-        leadingWidth: 70,
-        titleSpacing: 0,
-        leading: InkWell(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.arrow_back,
-                size: 24,
-              ),
-              CircleAvatar(
-                child: Image.asset(
-                  widget.chatModel.isGropup
-                      ? "assets/images/groups.png"
-                      : "assets/images/person.png",
-                ),
-                radius: 20,
-                backgroundColor: Colors.blueGrey,
-              )
-            ],
-          ),
-        ),
-        title: InkWell(
-          onTap: () {},
-          child: Container(
-            margin: EdgeInsets.all(5),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: Colors.blueGrey,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          backgroundColor: Color(0xFF075E54),
+          leadingWidth: 70,
+          titleSpacing: 0,
+          leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(widget.chatModel.name),
-                Text(
-                  "Last seen today at 12:05",
-                  style: TextStyle(fontSize: 15.5),
+                Icon(
+                  Icons.arrow_back,
+                  size: 24,
+                ),
+                CircleAvatar(
+                  child: Image.asset(
+                    widget.chatModel.isGropup
+                        ? "assets/images/groups.png"
+                        : "assets/images/person.png",
+                  ),
+                  radius: 20,
+                  backgroundColor: Colors.blueGrey,
                 )
               ],
             ),
           ),
+          title: InkWell(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.all(5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(widget.chatModel.name),
+                  Text(
+                    "Last seen today at 12:05",
+                    style: TextStyle(fontSize: 15.5),
+                  )
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.videocam),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.call),
+            ),
+            PopupMenuButton<String>(onSelected: (value) {
+              print(value);
+            }, itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  child: Text("View contact"),
+                  value: "View contact",
+                ),
+                PopupMenuItem(
+                  child: Text("Media, links, and docs"),
+                  value: "Media, links, and docs",
+                ),
+                PopupMenuItem(
+                  child: Text("Watsapp web"),
+                  value: "Whatsapp web",
+                ),
+                PopupMenuItem(
+                  child: Text("Search"),
+                  value: "Search",
+                ),
+                PopupMenuItem(
+                  child: Text("Mute notification"),
+                  value: "Mute notification",
+                ),
+              ];
+            })
+          ],
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.videocam),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.call),
-          ),
-          PopupMenuButton<String>(onSelected: (value) {
-            print(value);
-          }, itemBuilder: (BuildContext context) {
-            return [
-              PopupMenuItem(
-                child: Text("View contact"),
-                value: "View contact",
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            ListView(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Container(
+                      width: MediaQuery.of(context).size.width - 55,
+                      child: Card(
+                          margin: EdgeInsets.only(left: 2, right: 2, bottom: 8),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                          child: TextFormField())),
+                  CircleAvatar()
+                ],
               ),
-              PopupMenuItem(
-                child: Text("Media, links, and docs"),
-                value: "Media, links, and docs",
-              ),
-              PopupMenuItem(
-                child: Text("Watsapp web"),
-                value: "Whatsapp web",
-              ),
-              PopupMenuItem(
-                child: Text("Search"),
-                value: "Search",
-              ),
-              PopupMenuItem(
-                child: Text("Mute notification"),
-                value: "Mute notification",
-              ),
-            ];
-          })
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
